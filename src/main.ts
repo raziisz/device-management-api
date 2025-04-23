94c06eaf0b14088f471e829cdfe9b9dbd74d4a89
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-23 17:38:58 -0400

    feat: implements main config, pipe validation, swagger, cors and fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,62 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCors from '@fastify/cors';
+import {
+  ClassSerializerInterceptor,
+  UnprocessableEntityException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ClassValidatorFields } from './shared/domain/validators/class-validator-fields';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app
+    .getHttpAdapter()
+    .getInstance()
+    .register(fastifyCors, {
+      origin: '*',
+      methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: '*',
+    });
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.SWAGGER_TITLE)
+    .setDescription(process.env.SWAGGER_DESCRIPTION)
+    .setVersion(process.env.SWAGGER_VERSION)
+    .addServer(process.env.SWAGGER_API_URL)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new UnprocessableEntityException({
+          message: ClassValidatorFields.extractMessages(errors),
+          error: 'Unprocessable Entity',
+          statusCode: 422,
+        });
+      },
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
