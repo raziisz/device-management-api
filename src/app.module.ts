d4af5db5af4e2fc3187bc1ca823868209df1652d
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-23 18:52:45 -0400

    feat: created database module and prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, DatabaseModule],
   controllers: [AppController],
 })
 export class AppModule {}
