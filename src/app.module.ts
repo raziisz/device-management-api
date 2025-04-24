c6de37debaf530fb4ead8def52bada41dddc7916
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-23 21:35:55 -0400

    feat: Added category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
+import { CategoriesModule } from './categories/infrastructure/categories.module';
 
 @Module({
-  imports: [EnvConfigModule, DatabaseModule],
+  imports: [EnvConfigModule, DatabaseModule, CategoriesModule],
   controllers: [AppController],
 })
 export class AppModule {}
