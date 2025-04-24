2fe04cbed06a2cbf6474f684f4a9917b242cfa52
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-24 11:01:37 -0400

    feat: device output and unit test mapper

---
+++ src/devices/application/dtos/device.output.ts
@@ -0,0 +1,17 @@
+import { CategoryOutput } from '@/categories/application/dtos/category.output';
+import { DeviceEntity } from '@/devices/domain/entities/device.entity';
+
+export type DeviceOutput = {
+  id: number;
+  categoryId: number;
+  category?: CategoryOutput;
+  color: string;
+  partNumber: number;
+  createdAt: Date;
+};
+
+export class DeviceOutputMapper {
+  static toOutput(entity: DeviceEntity): DeviceOutput {
+    return entity.toJSON();
+  }
+}
