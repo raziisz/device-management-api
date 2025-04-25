e50cc610b7a878be4e7f0d3f1677a3f99675b8df
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-25 09:16:09 -0400

    feat: implements devices module

--- src/devices/infrastructure/devices.module.ts
+++ src/devices/infrastructure/devices.module.ts
@@ -1,4 +1,46 @@
 import { Module } from '@nestjs/common';
+import { DevicePrismaRepository } from './database/prisma/repositories/device-prisma.repository';
+import { PrismaService } from '@/shared/infrastructure/database/prisma/prisma.service';
+import { CreateDeviceUseCase } from '../application/usecase/create-device.usecase';
+import { DeviceRepository } from '../domain/repositories/device.repository';
+import { CategoryRepository } from '@/categories/domain/repositories/category.repository';
+import { ListDeviceUseCase } from '../application/usecase/list-device.usecase';
+import { DeleteDeviceUseCase } from '../application/usecase/delete-device.usecase';
+import { CategoriesModule } from '@/categories/infrastructure/categories.module';
 
-@Module({})
+@Module({
+  imports: [CategoriesModule],
+  providers: [
+    {
+      provide: 'PrismaService',
+      useClass: PrismaService,
+    },
+    {
+      provide: 'DeviceRepository',
+      useFactory: (prismaService: PrismaService) =>
+        new DevicePrismaRepository(prismaService),
+      inject: ['PrismaService'],
+    },
+    {
+      provide: CreateDeviceUseCase,
+      useFactory: (
+        deviceRepository: DeviceRepository,
+        categoryRepository: CategoryRepository,
+      ) => new CreateDeviceUseCase(deviceRepository, categoryRepository),
+      inject: ['DeviceRepository', 'CategoryRepository'],
+    },
+    {
+      provide: ListDeviceUseCase,
+      useFactory: (deviceRepository: DeviceRepository) =>
+        new ListDeviceUseCase(deviceRepository),
+      inject: ['DeviceRepository'],
+    },
+    {
+      provide: DeleteDeviceUseCase,
+      useFactory: (deviceRepository: DeviceRepository) =>
+        new DeleteDeviceUseCase(deviceRepository),
+      inject: ['DeviceRepository'],
+    },
+  ],
+})
 export class DevicesModule {}
